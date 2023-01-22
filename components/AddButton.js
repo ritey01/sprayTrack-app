60cd303c0ead89c61be7dfcc8c6e9afb4fb3767d
ritey01 <ritey01@gmail.com>
2023-01-22 14:27:23 +1300

    Add error handling

--- components/AddButton.js
+++ components/AddButton.js
@@ -1,31 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import styles from "../styles/AddButton.module.css";
 import { router } from "next/router";
 
 const AddButton = ({ propName, href, endpoint }) => {
+  const [error, setError] = useState("");
   const handleSubmit = async () => {
     if (propName) {
       // send request to the server
       try {
         const body = { name: propName };
 
-        await fetch(`/api/${endpoint}`, {
+        const result = await fetch(`/api/${endpoint}`, {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify(body),
         });
+        console.log(result);
         await router.replace(router.asPath);
       } catch (error) {
         console.error(error);
       }
     } else {
+      //need to create a state for this
       setError("Please enter a name");
       return;
     }
   };
   return (
     <div className={styles.buttonDisplay}>
+      {error && <p className={styles.error}>{error}</p>}
       <Link href={href} className={styles.addItemButton} onClick={handleSubmit}>
         Add
       </Link>
