12aa5117aecba596ca1f4526b2e7cad31687918f
ritey01 <ritey01@gmail.com>
2023-02-01 10:43:14 +1300

    Add check for existing data

--- pages/api/crop/postCrop.js
+++ pages/api/crop/postCrop.js
@@ -2,10 +2,25 @@ import prisma from "../../../lib/prisma";
 
 export default async function handler(req, res) {
   const { name } = req.body;
-  const result = await prisma.crops.create({
-    data: {
-      name: name,
-    },
-  });
-  res.status(201).json(result);
+  //checks if crop exists
+  const cropExists = !!(await prisma.crops.findFirst({
+    where: { name: name },
+  }));
+
+  if (cropExists) {
+    res.status(400).json({ message: "Crop already exists" });
+  }
+  if (req.method === "POST" && !cropExists) {
+    const result = await prisma.crops.create({
+      data: {
+        name: name,
+      },
+    });
+
+    res.status(201).json(result);
+  } else {
+    throw new Error(
+      `The HTTP ${req.method} method is not supported at this route.`
+    );
+  }
 }
