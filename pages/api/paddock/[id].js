71a5c487649c40c936d2a197e9527b5aa588b55d
ritey01 <ritey01@gmail.com>
2023-02-01 21:20:52 +1300

    text edit

--- pages/api/paddock/[id].js
+++ pages/api/paddock/[id].js
@@ -1,16 +1,13 @@
-import prisma from "../../../lib/prisma"
-
+import prisma from "../../../lib/prisma";
 
 export default async function handle(req, res) {
-  
-  
-  const paddockId = parseInt(req.query.id)
+  const paddockId = parseInt(req.query.id);
 
-  
   if (req.method === "DELETE") {
     const result = await prisma.paddock.delete({
       where: { id: paddockId },
     });
+
     res.status(200).json(result);
   } else {
     throw new Error(
