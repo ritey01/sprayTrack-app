34ac1d659266136594532bd7af4f00431755e7d3
ritey01 <ritey01@gmail.com>
2023-02-01 21:21:11 +1300

    Add delete

--- pages/api/spray/[id].js
+++ pages/api/spray/[id].js
@@ -1,1 +1,17 @@
-export default async function handle(req, res) {}
+import prisma from "../../../lib/prisma";
+
+export default async function handle(req, res) {
+  const sprayId = parseInt(req.query.id);
+
+  if (req.method === "DELETE") {
+    const result = await prisma.sprayList.delete({
+      where: { id: sprayId },
+    });
+
+    res.status(200).json(result);
+  } else {
+    throw new Error(
+      `The HTTP ${req.method} method is not supported at this route.`
+    );
+  }
+}
