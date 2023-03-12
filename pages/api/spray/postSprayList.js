3b15c617e038d68ea9bea41dcf685c4a7e3f61c4
ritey01 <ritey01@gmail.com>
2023-03-12 16:40:17 +1300

    Edit for sprayMix table and linked table

--- pages/api/spray/postSprayList.js
+++ pages/api/spray/postSprayList.js
@@ -3,35 +3,39 @@ import prisma from "../../../lib/prisma";
 export default async function sprayListHandler(req, res) {
   const { name, sprays } = req.body;
   console.log("✅", req.body);
-  //Checks if paddock exists
-  const sprayExists = !!(await prisma.sprayList.findFirst({
+  //Checks if sprayMix exists
+  const sprayExists = !!(await prisma.sprayMix.findFirst({
     where: { title: name },
   }));
 
   if (sprayExists) {
-    res.status(400).json({ message: "Spray mix already exists" });
+    res
+      .status(400)
+      .json({ message: "Spray mix with that name already exists" });
     return;
   }
   if (!sprayExists && req.method === "POST") {
     try {
-      const result = await prisma.sprayList.create({
+      const result = await prisma.sprayMix.create({
         data: {
           title: name,
-
-          sprayMix: {
-            createMany: {
-              data: sprays.map((spray) => {
-                return {
-                  spray: spray.spray,
+          sprays: {
+            create: sprays.map((spray) => ({
+              spray: {
+                create: {
                   rate: spray.rate,
                   unit: spray.unit,
-                };
-              }),
-            },
+                  sprayName: {
+                    connect: {
+                      id: spray.sprayId,
+                    },
+                  },
+                },
+              },
+            })),
           },
         },
       });
-
       console.log("🥵", result);
       res.status(201).json(result);
     } catch (err) {
