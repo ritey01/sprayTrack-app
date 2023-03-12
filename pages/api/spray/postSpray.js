d97906188091da3a0215e9a3d63768822407b7ed
ritey01 <ritey01@gmail.com>
2023-03-12 16:39:41 +1300

    Edit for sprayName table

--- pages/api/spray/postSpray.js
+++ pages/api/spray/postSpray.js
@@ -4,8 +4,8 @@ export default async function handler(req, res) {
   console.log("imade it");
   const { name } = req.body;
   console.log("🥵", name);
-  //Checks if paddock exists
-  const sprayExists = !!(await prisma.sprays.findFirst({
+  //Checks if spray exists
+  const sprayExists = !!(await prisma.sprayName.findFirst({
     where: { name: name },
   }));
 
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
     return;
   }
   if (!sprayExists && req.method === "POST") {
-    const result = await prisma.sprays.create({
+    const result = await prisma.sprayName.create({
       data: {
         name: name,
       },
