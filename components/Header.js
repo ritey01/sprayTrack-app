dbe4c5efb74adbfc81c4681baab64ebb2b9c51d3
ritey01 <ritey01@gmail.com>
2023-05-02 13:26:30 +1200

    Add header sign out

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,16 @@
+import Image from "next/image";
+import Link from "next/link";
+import { useSession, signIn, signOut } from "next-auth/react";
 import styles from "../styles/Header.module.css";
 import logo from "../assets/sprayerLogo.svg";
 import charLogo from "../assets/charLogo.png";
 import newLogo from "../assets/logo.png";
 import squLogo from "../assets/squLogo.png";
 import logoLong from "../assets/logoLong-resized.png";
-import Image from "next/image";
-import Link from "next/link";
 
 const Header = () => {
+  const { data: session } = useSession();
+
   return (
     <div className={styles.headerLayout}>
       <Image
@@ -26,6 +29,16 @@ const Header = () => {
         <Link href={"/paddock"} className={styles.paddockLink}>
           Start record
         </Link>
+        {/* sign out */}
+
+        {session ? (
+          <>
+            {/* <p> Signed in as {session.user.email} </p> */}
+            <button onClick={() => signOut()}>Sign out</button>
+          </>
+        ) : (
+          <button onClick={() => signIn("google")}>Sign in</button>
+        )}
       </div>
     </div>
   );
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,28 +1,60 @@
 import prisma from "../lib/prisma";
+import { getServerSession } from "next-auth/next";
+import { useSession } from "next-auth/react";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
+import Error from "./_error";
+import AccessDenied from "../components/accessDenied";
+import { authOptions } from "./api/auth/[...nextauth]";
 
-export async function getServerSideProps() {
+export async function getServerSideProps(context) {
+  let errorCode;
+  let sprayEvents;
+  const session = await getServerSession(context.req, context.res, authOptions);
+  if (!session) {
+    return {
+      props: {},
+    };
+  }
+  const companyId = session.user.companyId;
   // Fetch all SprayEvents and include related data from Paddock, Crop, SprayMix, and Company
-  const sprayEvents = await prisma.sprayEvent.findMany({
-    include: {
-      paddock: true,
-      crop: true,
-      sprayMix: {
-        include: {
-          sprays: {
-            include: {
-              spray: {
-                include: {
-                  sprayName: true,
+
+  try {
+    sprayEvents = await prisma.sprayEvent.findMany({
+      where: {
+        companyId: companyId,
+      },
+      include: {
+        paddock: true,
+        crop: true,
+        sprayMix: {
+          include: {
+            sprays: {
+              include: {
+                spray: {
+                  include: {
+                    sprayName: true,
+                  },
                 },
               },
             },
           },
         },
       },
-    },
-  });
+    });
 
-  //   Convert createdAt field to string
+    errorCode = context.res.statusCode > 200 ? context.res.statusCode : false;
+    //   Convert createdAt field to string
+  } catch (error) {
+    if (error instanceof PrismaClientKnownRequestError) {
+      console.error(error.code);
+    } else {
+      console.error(error);
+    }
+    context.res.statusCode = 500;
+    errorCode = context.res.statusCode;
+    sprayEvents = [];
+  }
+
   const sprayEventsWithCreatedAtAsString = sprayEvents.map((sprayEvent) => {
     return {
       ...sprayEvent,
@@ -33,45 +65,58 @@ export async function getServerSideProps() {
   return {
     props: {
       sprayEvents: sprayEventsWithCreatedAtAsString,
+      errorCode,
+      session,
     },
   };
 }
 
-export default function SprayEventDashboard({ sprayEvents }) {
-  return (
-    <div>
-      <h1>Spray Events</h1>
-      {sprayEvents.length == 0 ? (
-        <p>No spraying events recorded</p>
-      ) : (
-        <>
-          {/* This would be for mobile only */}
-          <table>
-            <thead>
-              <tr>
-                <th>Date</th>
-                <th>Paddock</th>
-                <th>Crop</th>
-                <th>Spray Mix</th>
-                <th>Created By</th>
-              </tr>
-            </thead>
-            <tbody>
-              {sprayEvents.map((sprayEvent) => (
-                <tr key={sprayEvent.id}>
-                  <td>{sprayEvent.date}</td>
-                  <td>{sprayEvent.paddock.name}</td>
-                  <td>{sprayEvent.crop.name}</td>
-                  <td>{sprayEvent.sprayMix.title}</td>
+export default function SprayEventDashboard({ sprayEvents, errorCode }) {
+  const { data: session } = useSession();
 
-                  <td>{sprayEvent.createdBy}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-          {/* Create another with all the details of SprayEvent displayed for desktop */}
-        </>
+  if (errorCode) {
+    return <Error statusCode={errorCode} />;
+  }
+  return (
+    <>
+      {session ? (
+        <div>
+          <h1>Spray Events</h1>
+          {sprayEvents.length == 0 ? (
+            <p>No spraying events recorded</p>
+          ) : (
+            <>
+              {/* This would be for mobile only */}
+              <table>
+                <thead>
+                  <tr>
+                    <th>Date</th>
+                    <th>Paddock</th>
+                    <th>Crop</th>
+                    <th>Spray Mix</th>
+                    <th>Created By</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {sprayEvents.map((sprayEvent) => (
+                    <tr key={sprayEvent.id}>
+                      <td>{sprayEvent.date}</td>
+                      <td>{sprayEvent.paddock.name}</td>
+                      <td>{sprayEvent.crop.name}</td>
+                      <td>{sprayEvent.sprayMix.title}</td>
+
+                      <td>{sprayEvent.createdBy}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+              {/* Create another with all the details of SprayEvent displayed for desktop */}
+            </>
+          )}
+        </div>
+      ) : (
+        <AccessDenied />
       )}
-    </div>
+    </>
   );
 }
